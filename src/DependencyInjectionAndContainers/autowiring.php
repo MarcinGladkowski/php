326db5d6335da0a864683b4dd3e1ad2ed073c5a5
Marcin <marcingladkowski@gmail.com>
2022-01-24 15:45:30 +0100

    uncomment TaxService

--- src/DependencyInjectionAndContainers/autowiring.php
+++ src/DependencyInjectionAndContainers/autowiring.php
@@ -10,13 +10,13 @@ use App\DependencyInjectionAndContainers\TaxService;
 require_once __DIR__ . '/../../vendor/autoload.php';
 
 $app = new App(true);
-//$taxService = $app::$container->get(TaxService::class);
-//var_dump(get_class($taxService));
-
+$taxService = $app::$container->get(TaxService::class);
+var_dump(get_class($taxService));
 
 $app::$container->set(PaymentServiceInterface::class, PaymentService::class);
 
 $invoiceService = $app::$container->get(InvoiceService::class);
 var_dump(get_class($invoiceService));
+
 $paymentService = $app::$container->get(PaymentServiceInterface::class);
 var_dump(get_class($paymentService));
