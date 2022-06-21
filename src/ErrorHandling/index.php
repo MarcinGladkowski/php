4fe3367a971e43277025250b9dd056484d002704
Marcin <marcingladkowski@gmail.com>
2022-06-21 13:25:10 +0200

    Error reporting

---
+++ src/ErrorHandling/index.php
@@ -0,0 +1,23 @@
+<?php
+
+
+// Predefined constants https://www.php.net/manual/en/errorfunc.constants.php
+var_dump(error_reporting()); // returns integer int(32767)
+
+function displayError($errno, $errstr, $errfile, $errline)
+{
+    echo 'In my system the error occurred on file ' . $errfile . "\n";
+}
+
+set_error_handler('displayError');
+
+// Set errors to E_ERROR level not display warning about trying access to undefined index
+//error_reporting(E_ERROR);
+
+$a['test'];
+
+//error_reporting(E_ALL);
+
+// Report all errors except E_NOTICE
+error_reporting(E_ALL & ~E_NOTICE);
+
